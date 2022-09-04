40d4e0ea4cf8a054f6086785cfe70660160858fe
Peter Dada <dapetoo@gmail.com>
2022-09-04 19:14:00 +0100

    Project initialization

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-06640050dc3f556bb"
+  instance_type = "t2.micro"
+  key_name      = "Rhel"
+
+  tags = {
+    Name = "WebServerInstance-IAC-Task"
+  }
+}
